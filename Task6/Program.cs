422a1a4dce7f9639c1bb367726eae7239f7a54a7
*Vasisualey* <basilnekrasov@mail.ru>
2023-02-03 22:02:22 +0300

    Решил задачу 6

---
+++ Task6/Program.cs
@@ -0,0 +1,24 @@
+﻿// 
+
+Console.Clear();
+int count = 1;
+while(count<= 100)
+{
+    if((count%3 == 0) & (count%5 == 0))
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if(count%3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else if(count%5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else
+    {
+        Console.WriteLine(count);
+    }
+    count++;
+}
