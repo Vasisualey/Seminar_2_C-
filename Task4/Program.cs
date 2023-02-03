d4907ce5e24d4613a649da358004edf64f2d78e3
*Vasisualey* <basilnekrasov@mail.ru>
2023-02-03 20:52:23 +0300

    Решил задачу 4

---
+++ Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число,
+// и проверяет, кратно ли оно одновременно 7 и 23
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number1 = int.Parse(Console.ReadLine());
+
+if(number1%7 == 0)
+{
+    if(number1%23 == 0)
+    {
+        Console.WriteLine($"{number1} является кратным 7 и 23 одновременно");
+    }
+    else
+    {
+        Console.WriteLine($"{number1} не является кратным 7 и 23 одновременно");
+    }
+}
+else
+{
+    Console.WriteLine($"{number1} не является кратным 7 и 23 одновременно");
+}
